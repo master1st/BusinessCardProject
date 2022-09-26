23409daddfb70fe6475204e90e242b60409fc404
seokhyun Yoon <gah1234@naver.com>
2022-09-26 15:48:55 +0900

    Card component
    Card component

--- cardmaker/cardmaker/src/components/Home/CardMaker/cardMaker.jsx
+++ cardmaker/cardmaker/src/components/Home/CardMaker/cardMaker.jsx
@@ -1,57 +1,59 @@
-import React, { useEffect } from 'react';
-import styles from './cardMaker.module.css';
-import Card from './cardEdit';
-import CardPreview from '../CardPreview/cardPreview';
-import { useState } from 'react';
+import React, { useEffect } from "react";
+import styles from "./cardMaker.module.css";
+import Card from "./cardEdit";
+import CardPreview from "../CardPreview/cardPreview";
+import { useState } from "react";
 const CardMaker = () => {
-    const [cardData, setcardData] = useState(
-    [
-    {'name' : "Ellie Dream Coding",
-    'company' : "Samsung Electronics",
-    'backgroundColor': "Dark",
-    'major' : "Software Engineer",
-    'email' : "dreamcoding.academe.com",
-    'introduction' : "Don't for get to code your dream",
-    'fileName':'',
-    'fileUrl':'',
+  const [cardData, setcardData] = useState([
+    {
+      id: 1,
+      name: "Ellie Dream Coding",
+      company: "Samsung Electronics",
+      backgroundColor: "Dark",
+      major: "Software Engineer",
+      email: "dreamcoding.academe.com",
+      introduction: "Don't for get to code your dream",
+      fileName: "",
+      fileUrl: "",
     },
-    {'name' : "Ellie Dream Coding",
-    'company' : "Samsung Electronics",
-    'backgroundColor': "Dark",
-    'major' : "Software Engineer",
-    'email' : "dreamcoding.academe.com",
-    'introduction' : "Don't for get to code your dream",
-    'fileName':'',
-    'fileUrl':'',
+    {
+      id: 2,
+      name: "Ellie Dream Coding",
+      company: "Samsung Electronics",
+      backgroundColor: "Dark",
+      major: "Software Engineer",
+      email: "dreamcoding.academe.com",
+      introduction: "Don't for get to code your dream",
+      fileName: "",
+      fileUrl: "",
     },
-    {'name' : "Ellie Dream Coding",
-    'company' : "Samsung Electronics",
-    'backgroundColor': "Dark",
-    'major' : "Software Engineer",
-    'email' : "dreamcoding.academe.com",
-    'introduction' : "Don't for get to code your dream",
-    'fileName':'',
-    'fileUrl':'',
-    }]
-    
-    );
-    /*useEffect(() => {
+    {
+      id: 3,
+      name: "Ellie Dream Coding",
+      company: "Samsung Electronics",
+      backgroundColor: "Dark",
+      major: "Software Engineer",
+      email: "dreamcoding.academe.com",
+      introduction: "Don't for get to code your dream",
+      fileName: "",
+      fileUrl: "",
+    },
+  ]);
+  /*useEffect(() => {
         
     })*/
 
-    return (
-        <section className={styles.box}>
-        <h1>Card Maker</h1>
-        <cardEdit />
-        <CardPreview cards={cardData}/>
-
-        </section>
-    );
+  return (
+    <section className={styles.box}>
+      <h1>Card Maker</h1>
+      <CardPreview cards={cardData} />
+    </section>
+  );
 };
 
 export default CardMaker;
 
 //textfield 2개, 색상 선택 1개,
-//textfield 2개 
-//textfield 1개 
-//btn 2개 
+//textfield 2개
+//textfield 1개
+//btn 2개
--- cardmaker/cardmaker/src/components/Home/card/card.jsx
+++ cardmaker/cardmaker/src/components/Home/card/card.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Card = (props) => {
+
+    console.log(props);
     return (
         <div>
             
